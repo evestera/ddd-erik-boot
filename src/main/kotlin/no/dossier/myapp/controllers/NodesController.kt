0c23c0aa53c4f5399b2c2261711f2aa9c8998e25
Erik Vesteraas <erik.vesteraas@dossier.no>
2020-11-25 09:47:18 +0100

    RestController

--- src/main/kotlin/no/dossier/myapp/controllers/NodesController.kt
+++ src/main/kotlin/no/dossier/myapp/controllers/NodesController.kt
@@ -3,14 +3,14 @@ package no.dossier.myapp.controllers
 import no.dossier.myapp.domain.Node
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
 
-@Controller
+@RestController
 class NodesController(
     private val restTemplate: RestTemplate
 ) {
@@ -23,7 +23,7 @@ class NodesController(
   @PostMapping("/notify")
   fun postNotify(@RequestBody body: NotifyBody): ResponseEntity<out Any> {
     val node = Node(url = body.url)
-    val response = restTemplate.getForEntity<Health>(node.url)
+    val response = restTemplate.getForEntity<Health>(node.url + "/health")
     if (!response.statusCode.is2xxSuccessful) {
       return ErrorWrapper.genericBadRequest("Got response from node, but status was not 2xx").toResponseEntity()
     }
--- src/main/kotlin/no/dossier/myapp/controllers/WidgetsController.kt
+++ src/main/kotlin/no/dossier/myapp/controllers/WidgetsController.kt
@@ -1,9 +1,9 @@
 package no.dossier.myapp.controllers
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class WidgetsController {
 
   @GetMapping("/widgets")
