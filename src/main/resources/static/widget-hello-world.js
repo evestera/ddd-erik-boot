b39da9be7c98a31b04c39b471eb7e67a03599502
Erik Vesteraas <erik.vesteraas@dossier.no>
2020-11-26 01:24:07 +0100

    Scoping thing

--- src/main/resources/static/widget-hello-world.js
+++ src/main/resources/static/widget-hello-world.js
@@ -1,6 +1,8 @@
-const selector = '.widget-erik-boot-hello-world';
-if (document.querySelector(selector)) {
-  import('https://unpkg.com/htm/preact/standalone.module.js').then(({html, render}) => {
-    render(html`<p>Hello world!</p>`, document.querySelector(selector))
-  });
+{
+  const selector = '.widget-erik-boot-hello-world';
+  if (document.querySelector(selector)) {
+    import('https://unpkg.com/htm/preact/standalone.module.js').then(({html, render}) => {
+      render(html`<p>Hello world!</p>`, document.querySelector(selector))
+    });
+  }
 }
