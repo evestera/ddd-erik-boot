379d2f995e3591e95899c0db1806d65d832ac1ee
Erik Vesteraas <erik.vesteraas@dossier.no>
2020-11-27 18:22:30 +0100

    Add name and description to widget

--- src/main/kotlin/no/dossier/myapp/controllers/WidgetsController.kt
+++ src/main/kotlin/no/dossier/myapp/controllers/WidgetsController.kt
@@ -10,6 +10,8 @@ class WidgetsController {
   @GetMapping("/widgets")
   fun getWidgetsList(): List<Widget> = listOf(
       Widget(
+          name = "Hello World",
+          description = "A widget that just renders the message 'Hello World'",
           selector = ".widget-erik-boot-hello-world",
           script = "https://ddd-erik-boot.herokuapp.com/widget-hello-world.js"
       )
--- src/main/kotlin/no/dossier/myapp/domain/Widget.kt
+++ src/main/kotlin/no/dossier/myapp/domain/Widget.kt
@@ -2,5 +2,7 @@ package no.dossier.myapp.domain
 
 class Widget(
     val selector: String,
-    val script: String
+    val script: String,
+    val description: String? = null,
+    val name: String? = null
 )
